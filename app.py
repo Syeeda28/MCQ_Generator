51d7da6ee3b7cf3f442cb4b83eac08c6fc27ef21
safiya <syeedasafiya28@gmail.com>
2026-07-23 00:36:54 +0530

    Final project before deployment

--- app.py
+++ app.py
@@ -187,4 +187,4 @@ if __name__ == "__main__":
     os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
     os.makedirs(app.config['RESULTS_FOLDER'], exist_ok=True)
 
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=False)
